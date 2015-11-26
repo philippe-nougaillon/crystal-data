8feaa3fcf3e99331276499de1f84b7fec921bd76
philippe-nougailon <philippe.nougaillon@gmail.com>
2015-11-26 17:11:54 +0100

    welcome screens

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,14 +10,14 @@ class UsersController < ApplicationController
     end
 
     def create
-	    user = User.new(user_params)
-	    if user.save
-	      #update_authentication_token(user, nil)
-	      session[:user_id] = user.id
+	    @user = User.new(user_params)
+	    if @user.save
+	      session[:user_id] = @user.id
 	      cookies.permanent[:auth_token] = nil
-	      redirect_to '/', notice:"Bienvenue '#{user.name}' !"
+	      redirect_to '/', notice:"Bienvenue '#{@user.name}' !"
 	    else
-	      redirect_to '/signup'
+		    render :new	
+	      #redirect_to '/signup'
 	    end
 	end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :tables
 
   validates :name, :email, :password, :password_confirmation, presence:true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create	
 
 end
