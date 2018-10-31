f6f8d9cfd9ab45c5de12b68b9e3c9912d13fa7dc
philippe-nougailon <philippe.nougaillon@gmail.com>
2018-10-31 11:31:36 +0100

    sitemap install

--- sitemap.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'sitemap_generator'
-
-SitemapGenerator::Sitemap.default_host = 'http://www.crystaldata.net'
-SitemapGenerator::Sitemap.create do
-  add '/about', :changefreq => 'weekly'
-end
-SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
